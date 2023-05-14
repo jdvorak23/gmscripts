10c2fb2d4e66e76c09eedd7dbcfeae16e2a235b3
jdvorak23 <dvorakj3@post.cz>
2023-05-14 13:55:23 +0200

    Kopírování + terminál

--- itnetwork.js
+++ itnetwork.js
@@ -7,8 +7,21 @@
 
 document.addEventListener('copy', ev => {
     let text = window.getSelection().toString();
-    navigator.clipboard.writeText(text).then(() => {
-    },() => {
-        console.error('Failed to copy');
-    });
+    navigator.clipboard.writeText(text);
 });
+
+for(let pre of [... document.querySelectorAll('pre')]){
+    if(pre.innerText.includes("\n"))
+        continue;
+    let copyButton = document.createElement('span');
+    copyButton.innerHTML = '<i class="fa fa-clone fa-2x" aria-hidden="true" style="cursor: pointer"></i>';
+    copyButton.style.position = "absolute";
+    copyButton.style.right = "5px";
+    copyButton.style.top = "5px";
+    let textToCopy = pre.innerText.trim();
+    copyButton.addEventListener('click', ()=>{
+        navigator.clipboard.writeText(textToCopy);
+    });
+    pre.style.position = "relative";
+    pre.appendChild(copyButton);
+}
